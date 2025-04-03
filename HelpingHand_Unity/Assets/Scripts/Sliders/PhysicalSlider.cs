055f1c172c2139aac8d82a513710dd8273d1b2f6
p.luneau <paul.luneau@mail.com>
2025-04-03 16:59:14 +0200

    Semi working Physical slider

--- HelpingHand_Unity/Assets/Scripts/Sliders/PhysicalSlider.cs
+++ HelpingHand_Unity/Assets/Scripts/Sliders/PhysicalSlider.cs
@@ -1,13 +1,29 @@
 using System;
 
+using Sirenix.OdinInspector.Editor.Validation;
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PhysicalSlider : MonoBehaviour
 {
     public event Action<float> OnValueChanged;
 
+    [SerializeField] private InputActionReference m_sliderAction;
+
+
+    private void Awake()
+    {
+        m_sliderAction.action.performed += OnSliderValueChanged;
+    }
+
     public void SetSliderValue(float value)
     {
         // TODO
+    }
+
+    private void OnSliderValueChanged(InputAction.CallbackContext context)
+    {
+        OnValueChanged?.Invoke(context.action.ReadValue<float>());
     }
 }
