45a17bccdb8ac58db799a1db78815cb2ae2e0183
BRS0T <spam.brst@gmail.com>
2025-04-08 17:40:27 +0200

    UIEventSounds WIP

--- HelpingHand_Unity/Assets/Sounds/WwiseScripts/UIEventSounds.cs
+++ HelpingHand_Unity/Assets/Sounds/WwiseScripts/UIEventSounds.cs
@@ -19,6 +19,10 @@ public class UIEventSounds : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     public Slider Slider_1;
     float previousSliderValue = -1f;
     bool hasplayed=false;
+    bool isSliderSoundPlaying = false;
+
+    private uint playingId = AkSoundEngine.AK_INVALID_PLAYING_ID; //ID de lecture de l'évenement
+
 
     public void OnPointerDown(PointerEventData eventData)
     {
@@ -43,35 +47,64 @@ public class UIEventSounds : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
 
     public void TriggerSliderValueChange()
     {
-        if (Mathf.Abs(Slider_1.value - previousSliderValue) > 0.01f) // small threshold to avoid float noise
+        OnPointerUpSlider();
+        if (Mathf.Abs(Slider_1.value - previousSliderValue) > 0.01f)
         {
             Debug.Log(Slider_1.value);
             RTPC_Slider.SetValue(null, Slider_1.value);
-            PlaySliderSfx.Post(gameObject);
+
+            if (!isSliderSoundPlaying)
+            {
+                isSliderSoundPlaying = true;
+                playingId = PlaySliderSfx.Post(gameObject, (uint)AkCallbackType.AK_EndOfEvent, SliderSoundEnded);
+            }
 
             previousSliderValue = Slider_1.value;
         }
-        else
+
+        if (Slider_1.value == Slider_1.maxValue)
         {
-            StopSliderSfx.Post(gameObject);
-
+            OnSliderMaxValue.Post(gameObject);
+            Debug.Log("MaxValue!");
         }
+        else if (Slider_1.value == Slider_1.minValue)
+        {
+            OnSliderMinValue.Post(gameObject);
+            Debug.Log("MinValue!");
 
-
-            if (Slider_1.value == Slider_1.maxValue)
-            {
-                OnSliderMaxValue.Post(gameObject);
-                Debug.Log("MaxValue!");
-            }
-            else if (Slider_1.value == Slider_1.minValue)
-            {
-                OnSliderMinValue.Post(gameObject);
-                Debug.Log("MinValue!");
-            }
         }
     }
 
+    private void SliderSoundEnded(object in_cookie, AkCallbackType in_type, object in_info)
+    {
+        if (in_type == AkCallbackType.AK_EndOfEvent)
+        {
+            isSliderSoundPlaying = false;
+        }
+    }
 
+    // Appelée quand la valeur du slider change
+    public void TriggerSliderSeek()
+    {
+        if (playingId != AkSoundEngine.AK_INVALID_PLAYING_ID)
+        {
+            float sliderValue = Slider_1.value;
+            float percentage = Mathf.InverseLerp(Slider_1.minValue, Slider_1.maxValue, sliderValue); // convert to 0.0 - 1.0
+
+            // Seek to the corresponding position in the sound
+            AkSoundEngine.SeekOnEvent(PlaySliderSfx.Id, gameObject, percentage, false);
+        }
+    }
+
+    public void OnPointerUpSlider()
+    {
+        Debug.Log("Slider handle released.");
+        StopSliderSfx.Post(gameObject);
+        
+    }
+
+
+}
 
 
 
