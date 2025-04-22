fc3967f2064d120a607a121e4a96a43cc9718a05
Matthieu <matthieu.golsenne@gmail.com>
2025-04-22 13:22:10 +0200

    Added "Min" & "Max" labels to the interactive object editor

--- HelpingHand_Unity/Assets/Scripts/Editor/InteractiveObjectTestEditor.cs
+++ HelpingHand_Unity/Assets/Scripts/Editor/InteractiveObjectTestEditor.cs
@@ -3,7 +3,6 @@ using System;
 using UnityEditor;
 
 using UnityEngine;
-using UnityEngine.UIElements;
 
 using UnityUtility.Extensions;
 
@@ -12,14 +11,32 @@ public class InteractiveObjectTestEditor : Editor
 {
     [NonSerialized] private Tool m_currentTool;
 
+    private GUIStyle m_style;
+    
     private void Awake()
     {
         m_currentTool = Tools.current;
         Tools.current = Tool.None;
     }
 
+    private GUIStyle GetStyle()
+    {
+        GUIStyle style = new GUIStyle("ProfilerBadge");
+        style.alignment = TextAnchor.UpperLeft;
+        var padding = style.padding;
+        padding.left = 4;
+        padding.right = 0;
+        style.padding = padding;
+        return style;
+    }
+
     protected virtual void OnSceneGUI()
     {
+        if (m_style == null)
+        {
+            m_style = GetStyle();
+        }
+        
         if (Tools.current == Tool.Move)
         {
             Tools.current = Tool.None;
@@ -30,7 +47,10 @@ public class InteractiveObjectTestEditor : Editor
         float gridSize = PuppetSettings.Instance.TileSize;
 
         EditorGUI.BeginChangeCheck();
+
         Vector3 newStartPosition = Handles.PositionHandle(interactiveObject.StartPosition, Quaternion.identity).Snap(gridSize);
+        Handles.Label(newStartPosition + new Vector3(.05f, -0.05f, 0), new GUIContent("Min"), m_style);
+        
         if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(interactiveObject, "Change Start Position");
@@ -39,7 +59,10 @@ public class InteractiveObjectTestEditor : Editor
         }
 
         EditorGUI.BeginChangeCheck();
+        
         Vector3 newEndPosition = Handles.PositionHandle(interactiveObject.EndPosition, Quaternion.identity).Snap(gridSize);
+        Handles.Label(newEndPosition+ new Vector3(.05f, -0.05f, 0), new GUIContent("Max"), m_style);
+        
         if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(interactiveObject, "Change End Position");
