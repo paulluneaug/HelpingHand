e4f77a94a5b5635b1b2f0b2492e9d7d5411a0d94
Paul Luneau <paul.luneau@mail.com>
2025-05-05 19:36:21 +0200

    Virtual potentiometer progress

--- HelpingHand_Unity/Assets/Scripts/VirtualController/VirtualPotentiometer.cs
+++ HelpingHand_Unity/Assets/Scripts/VirtualController/VirtualPotentiometer.cs
@@ -1,16 +1,74 @@
-using UnityEngine;
+using System;
 
-public class VirtualPotentiometer : MonoBehaviour
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+using UnityUtility.Extensions;
+
+[RequireComponent(typeof(RectTransform))]
+public class VirtualPotentiometer : UIBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler, IInitializePotentialDragHandler, ICanvasElement
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region Interface implementation
+    public void GraphicUpdateComplete()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LayoutComplete()
     {
+    }
+
+    public void OnInitializePotentialDrag(PointerEventData eventData)
+    {
+        eventData.useDragThreshold = false;
+    }
+
+    public void Rebuild(CanvasUpdate executing)
+    {
+    }
+    #endregion
+
+
+
+    [SerializeField] private float m_originAngle;
+    [SerializeField] private float m_range;
+
+    [NonSerialized] private float m_value;
+
+    [NonSerialized] private RectTransform m_rectTransform;
+    [NonSerialized] private Vector2 m_dragStartPosition;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        m_value = 0.0f;
+
+        m_rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+        m_dragStartPosition = ToLocalPosition(eventData.position);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 currentLocalPosition = ToLocalPosition(eventData.position);
+
+        Vector2 offset = currentLocalPosition - m_dragStartPosition;
+
+
         
+
+    }
+
+    private Vector2 ToLocalPosition(Vector2 position)
+    {
+        return position - m_rectTransform.pivot;
     }
 }
