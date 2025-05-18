613ebada5385536e90c035e987d04c133253de9c
Matthieu <matthieu.golsenne@gmail.com>
2025-05-18 20:13:50 +0200

    Added debug "raise" button for game events

--- HelpingHand_Unity/Assets/Scripts/Events/GameEvents/GameEvent.cs
+++ HelpingHand_Unity/Assets/Scripts/Events/GameEvents/GameEvent.cs
@@ -1,9 +1,16 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+
+using UnityEngine;
 
 namespace Events
 {
     [CreateAssetMenu(menuName = "Scriptable Objects/Events/Game Event")]
     public class GameEvent : BaseGameEvent
     {
+        [Button("Raise")]
+        private void Internal_Raise()
+        {
+            Raise();
+        }
     }
 }
