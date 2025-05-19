6aad58bb5b6b9928008dccce46b39ecbe9028047
Matthieu <matthieu.golsenne@gmail.com>
2025-05-19 18:30:33 +0200

    Added a screen manager
    For showing the virtual console on the second screen

---
+++ HelpingHand_Unity/Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log ("displays connected: " + Display.displays.Length);
+        for (int i = 1; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }    
+    }
+}
