783a6b07f5ae319b28dab19f45e24dfc2359e65b
Matthieu <matthieu.golsenne@gmail.com>
2025-05-23 02:46:15 +0200

    Split State Setter into State Listener for the callbacks

--- HelpingHand_Unity/Assets/Scripts/EntityStateSetter.cs
+++ HelpingHand_Unity/Assets/Scripts/EntityStateSetter.cs
@@ -1,7 +1,6 @@
 using Sirenix.OdinInspector;
 
 using UnityEngine;
-using UnityEngine.Events;
 
 public class EntityStateSetter : SerializedMonoBehaviour
 {
@@ -11,34 +10,11 @@ public class EntityStateSetter : SerializedMonoBehaviour
     [SerializeField][BoxGroup][PropertySpace(4, 4)]
     private ConditionBase m_condition = new ConditionNone();
 
-    [SerializeField][FoldoutGroup("Callbacks")]
-    private UnityEvent m_onStateSet;
-
-    [SerializeField][FoldoutGroup("Callbacks")]
-    private UnityEvent m_onStateUnset;
-
-    [SerializeField][FoldoutGroup("Callbacks")]
-    private UnityEvent<bool> m_onStateChanged;
-
     private void Start()
     {
         m_condition.Initialize();
         m_condition.OnPreconditionUpdated += OnConditionUpdated;
         m_state.SetValueWithoutNotify(m_condition.Test());
-        m_state.AddListener(OnValueChanged);
-    }
-
-    private void OnValueChanged(bool value)
-    {
-        if (value)
-        {
-            m_onStateSet.Invoke();
-        }
-        else
-        {
-            m_onStateUnset.Invoke();
-        }
-        m_onStateChanged.Invoke(value);
     }
 
     private void OnConditionUpdated()
