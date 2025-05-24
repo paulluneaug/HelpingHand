cd50aa8db2ef8fc336313b997380581fd20f1d38
Matthieu <matthieu.golsenne@gmail.com>
2025-05-24 21:05:46 +0200

    Added simple blackboard singleton

--- HelpingHand_Unity/Assets/Scripts/DialogueGraph/GraphBlackboard.cs
+++ HelpingHand_Unity/Assets/Scripts/DialogueGraph/GraphBlackboard.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 
+using Sirenix.OdinInspector;
 using Sirenix.Serialization;
 
 using UnityEngine;
@@ -9,9 +11,31 @@ using UnityUtility.Singletons;
 
 public class GraphBlackboard : MonoBehaviourSingleton<GraphBlackboard>
 {
-    [OdinSerialize] [SerializeField]
-    private Dictionary<string, object> m_blackboard = new();
+    [OdinSerialize] [SerializeField] [ReadOnly]
+    private SerializedDictionary<string, object> m_blackboard = new();
 
-    public Dictionary<string, object> Blackboard => m_blackboard;
-    
+    public SerializedDictionary<string, object> Blackboard => m_blackboard;
+
+    public bool TryGetValue<T>(string key, out T result)
+    {
+        if (m_blackboard.TryGetValue(key, out object objectValue))
+        {
+            if (objectValue is T value)
+            {
+                result = value;
+                return true;
+            }
+            else
+            {
+                throw new InvalidCastException($"Blackboard value corresponding to [{key}] is not of type {typeof(T).Name}");
+            }
+        }
+        else
+        {
+            throw new KeyNotFoundException($"Cannot find [{key}] in the blackboard");
+        }
+
+        result = default;
+        return false;
+    }
 }
