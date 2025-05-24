b9bc5d5af5419369187b7f37564ccdbf0c146081
Matthieu <matthieu.golsenne@gmail.com>
2025-05-24 21:07:07 +0200

    Added a wait for frames node

---
+++ HelpingHand_Unity/Assets/Scripts/DialogueGraph/Nodes/WaitFrameNode.cs
@@ -0,0 +1,77 @@
+using System;
+
+using Cysharp.Threading.Tasks;
+
+using Sirenix.OdinInspector;
+
+using UnityEngine;
+
+using XNode;
+[CreateNodeMenu("Waiting/Wait Frame")] [NodeTint(0.2f, 0.1f, .3f)]
+public class WaitFrameNode : InterruptableNode
+{
+    private enum WaitType
+    {
+        [LabelText("Frame count")] FrameCount,
+        [LabelText("Next frame")] NextFrame,
+        [LabelText("End of frame")] EndOfFrame,
+        [LabelText("LateUpdate")] LateUpdate,
+    }
+    
+    [Input]
+    public DialogueFlow m_in;
+
+    [Output]
+    public DialogueFlow m_out;
+
+    [SerializeField] [LabelWidth(50)]
+    private WaitType m_type;
+
+    [SerializeField] [LabelWidth(100)] [ShowIf("@m_type == WaitType.FrameCount")]
+    private int m_frameCount;
+
+    protected override void Init()
+    {
+        base.Init();
+        m_description = "Wait for n seconds before resuming the flow. Loops back if interrupted";
+    }
+    
+    protected override async UniTask ExecuteNode(GraphRunnerHandler handler, NodePort inPort)
+    {
+        while (true)
+        {
+            UniTask task;
+            switch (m_type)
+            {
+                case WaitType.FrameCount:
+                    DebugLog($"Waiting for {m_frameCount} frames");
+                    task = UniTask.DelayFrame(m_frameCount, PlayerLoopTiming.Update, handler.StopToken);
+                    break;
+                case WaitType.NextFrame:
+                    DebugLog($"Waiting for next frame");
+                    task = UniTask.NextFrame(handler.StopToken);
+                    break;
+                case WaitType.EndOfFrame:
+                    DebugLog($"Waiting for end of frame");
+                    task = UniTask.WaitForEndOfFrame(handler.StopToken); 
+                    break;
+                case WaitType.LateUpdate:
+                    task = UniTask.Yield(PlayerLoopTiming.PostLateUpdate, handler.StopToken);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            if (await task.SuppressCancellationThrow())
+            {
+                DebugLog($"Wait interrupted");
+                // The graph is being paused => We have to wait its reactivation
+                await HandlePauseStop(handler);
+                continue;
+            }
+            
+            DebugLog($"Wait done");
+            break;
+        }
+    }
+}
