288eaa07a511d61d45f0a049515077774b2c60bd
p.luneau <paul.luneau@mail.com>
2025-05-31 21:06:43 +0200

    Added a DialogueReadMode option in the optionsManager

--- HelpingHand_Unity/Assets/Scripts/Managers/GameOptionsManager.cs
+++ HelpingHand_Unity/Assets/Scripts/Managers/GameOptionsManager.cs
@@ -8,4 +8,5 @@ public class GameOptionsManager
     public ObservableField<bool> IsHighContrast = new ObservableField<bool>(false);
     public ObservableField<float> GameSpeed = new ObservableField<float>(1.0f);
     public ObservableField<bool> IsWindowed = new ObservableField<bool>(false);
+    public ObservableField<DialogueReadMode> DialogueReadMode = new ObservableField<DialogueReadMode>(global::DialogueReadMode.Auto);
 }
---
+++ HelpingHand_Unity/Assets/Scripts/UI/Options/OptionsEnum/DialogueReadMode.cs
@@ -0,0 +1,5 @@
+public enum DialogueReadMode
+{
+    Manual,
+    Auto,
+}
