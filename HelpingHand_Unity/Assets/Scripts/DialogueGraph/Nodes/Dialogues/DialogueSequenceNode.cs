6d97ae538e18da32b7b32283c4b088e49115111a
Matthieu <matthieu.golsenne@gmail.com>
2025-06-01 12:43:43 +0200

    Added an option to choose randomly in a dialogue sequence

--- HelpingHand_Unity/Assets/Scripts/DialogueGraph/Nodes/Dialogues/DialogueSequenceNode.cs
+++ HelpingHand_Unity/Assets/Scripts/DialogueGraph/Nodes/Dialogues/DialogueSequenceNode.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -28,9 +29,12 @@ public class DialogueSequenceNode : BaseNode
     [SerializeField]
     private bool m_loop;
 
+    [SerializeField]
+    private bool m_random;
+
     private NodePort[] m_orderedNodePorts;
     private int m_sequenceIndex;
-
+    private Queue<int> m_randomQueue;
     
     protected override string Infos => "Continue le flow vers le premier dialogue de la séquence qui n'a pas été lu.\n" +
                                        "Peut boucler après avoir lu tous les dialogues (ou sort dans \"else\").\n" +
@@ -40,20 +44,20 @@ public class DialogueSequenceNode : BaseNode
     {
         m_sequenceIndex = -1;
         m_orderedNodePorts = DynamicOutputs.OrderBy(p => p.fieldName).ToArray();
+        InitializeRandomQueue();
+    }
+
+    private void InitializeRandomQueue()
+    {
+        System.Random rnd = new (Guid.NewGuid().GetHashCode());
+        m_randomQueue = new Queue<int>(Enumerable.Range(0, m_orderedNodePorts.Length).OrderBy(r => rnd.Next()));
     }
 
     protected override async UniTask ContinueFlow(GraphRunnerHandler handler, NodePort inPort)
     {
         List<NodePort> continuePorts = new();
 
-        m_sequenceIndex++;
-        
-        if (m_loop)
-        {
-            m_sequenceIndex %= m_orderedNodePorts.Length;
-        }
-
-        if (m_sequenceIndex < m_orderedNodePorts.Length)
+        if (TryGetNextSequenceIndex(ref m_sequenceIndex))
         {
             NodePort outputPort = m_orderedNodePorts[m_sequenceIndex];
 
@@ -76,5 +80,39 @@ public class DialogueSequenceNode : BaseNode
 
         DebugLog($"Reading Dialogues: [{continuePorts.Select(port => GetConnectedNodesToPort(port).Select(node => (node as DialogueNode).name).Aggregate((dialogue, aggregate) => $"{aggregate}, {dialogue}")).Aggregate((dialogues, aggregate) => $"{aggregate}, {dialogues}")}]");
         await UniTask.WhenAll(continuePorts.Select(port => ContinueFlow(handler, inPort, port)));
+    }
+
+    private bool TryGetNextSequenceIndex(ref int sequenceIndex)
+    {
+        if (m_random)
+        {
+            if (m_randomQueue.TryDequeue(out sequenceIndex))
+            {
+                return true;
+            } 
+            else 
+            {
+                if (m_loop)
+                {
+                    InitializeRandomQueue();
+                    sequenceIndex = m_randomQueue.Dequeue();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            sequenceIndex++;
+            if (m_loop)
+            {
+                sequenceIndex %= m_orderedNodePorts.Length;
+            }
+
+            return sequenceIndex < m_orderedNodePorts.Length;
+        }
     }
 }
