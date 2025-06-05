dbac6905466679113bb6dcfd7be38307ea5eee50
p.luneau <paul.luneau@mail.com>
2025-06-05 17:20:28 +0200

    Added a script to control the audio of an object driven by a float variable

---
+++ HelpingHand_Unity/Assets/Scripts/Audio/ObjectsAudioControllers/FloatVariableObjectAudio.cs
@@ -0,0 +1,25 @@
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+using UnityEngine;
+
+public class FloatVariableObjectAudio : SerializedMonoBehaviour
+{
+    [SerializeField] private IBaseVariableContainer<float> m_variableContainer;
+    [SerializeField] private FloatObjectAudioContainer m_audioContainer;
+
+    private void Start()
+    {
+        m_audioContainer.Init(m_variableContainer.Variable, gameObject);
+    }
+
+    private void Update()
+    {
+        m_audioContainer.Update(Time.deltaTime);
+    }
+
+    private void OnDestroy()
+    {
+        m_audioContainer.Dispose();
+    }
+}
