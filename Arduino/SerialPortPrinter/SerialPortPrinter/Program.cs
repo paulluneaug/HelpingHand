3df39399d5e30cd15b7436f884557f5c5e40c25c
Paul Luneau <paul.luneau@mail.com>
2025-06-24 15:19:16 +0200

    SerialPort printer

---
+++ Arduino/SerialPortPrinter/SerialPortPrinter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialPortPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Available Ports : ");
+            foreach (string portName in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                Console.WriteLine($" - {portName}");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
