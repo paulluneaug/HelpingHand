175f3d904da0f39be0d16221341ea4e31f7c0b90
BRS0T <spam.brst@gmail.com>
2025-06-25 11:56:38 +0200

    Regenerated Soundbanks

--- HelpingHand_Unity/Assets/Scripts/Audio/Managers/EventManager.cs
+++ HelpingHand_Unity/Assets/Scripts/Audio/Managers/EventManager.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+
 using Sirenix.OdinInspector;
 
 using UnityEngine;
@@ -92,9 +94,17 @@ public class EventManager : MonoBehaviour
 
     #endregion
 
+    #region Ambience Events
+    [FoldoutGroup("Ambience Events/Gameplay")][LabelWidth(200)] public WwiseEvent Town_Amb_Play;
+
+    [FoldoutGroup("Ambience Events/Gameplay")][LabelWidth(200)] public WwiseEvent Tavern_Amb_Play;
+    [FoldoutGroup("Ambience Events/Gameplay")][LabelWidth(200)] public WwiseEvent Armurerie_Amb_Play;
+    [FoldoutGroup("Ambience Events/Gameplay")][LabelWidth(200)] public WwiseEvent Supermarket_Amb_Play;
+
+    [FoldoutGroup("Ambience Events/Gameplay")][LabelWidth(200)] public WwiseEvent Volcano_Amb_Play;
+    #endregion
     #region ItemDropper Events 
     [FoldoutGroup("ItemDropper Events")][LabelWidth(200)] public WwiseEvent Play_FallingObject;
-
     [FoldoutGroup("ItemDropper Events")][LabelWidth(200)] public WwiseEvent Play_HatCatch;
     [FoldoutGroup("ItemDropper Events")][LabelWidth(200)] public WwiseEvent Play_WeaponCatch;
 
