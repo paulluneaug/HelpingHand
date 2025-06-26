e4547acabbe3b163a6b7d9f42a10af1f1ecf4835
Matthieu <matthieu.golsenne@gmail.com>
2025-06-26 05:21:53 +0200

    Fixed combat music selection

--- HelpingHand_Unity/Assets/Scripts/Audio/MusicController.cs
+++ HelpingHand_Unity/Assets/Scripts/Audio/MusicController.cs
@@ -4,8 +4,6 @@ using Sirenix.OdinInspector;
 
 using UnityEngine;
 
-using WwiseEvent = AK.Wwise.Event;
-
 public class MusicController : MonoBehaviour
 {
     [Serializable]
@@ -13,7 +11,6 @@ public class MusicController : MonoBehaviour
     {
         [SerializeField] private EntityState m_musicPlayingState;
         [SerializeField] private EntityState m_musicPlayedState;
-
         [SerializeField] private MusicState m_musicState;
 
         public void SetIsPlaying(bool isPlaying)
@@ -21,15 +18,14 @@ public class MusicController : MonoBehaviour
             m_musicPlayingState.Value = isPlaying;
         }
 
-        public void SwitchToMusic(GameObject go)
+        public void Play()
         {
             AudioManager.Instance.StateManager.SetMusicState(m_musicState);
-            //_ = m_switchToMusic.Post(go);
         }
-
     }
 
     [Title("Input Events")]
+    [SerializeField] private RotaryEncoderInputEvent m_selectMusicInput;
     [SerializeField] private BaseVariable<bool> m_selectedMusic0;
     [SerializeField] private BaseVariable<bool> m_selectedMusic1;
     [SerializeField] private ButtonInputEvent m_playMusicEvent;
@@ -38,7 +34,8 @@ public class MusicController : MonoBehaviour
     [SerializeField, RequiredListLength(MinLength = 4, MaxLength = 4)] private MusicStates[] m_states = new MusicStates[4];
 
     // Cache
-    [NonSerialized] private int m_selectedMusic = 0;
+    [NonSerialized] private int m_selectedMusicIndex = -1;
+    [NonSerialized] private int m_playingMusicIndex = -1;
     [NonSerialized] private bool m_playingMusic = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -67,68 +64,48 @@ public class MusicController : MonoBehaviour
         }
 
         Init();
-
     }
 
     private void Init()
     {
-        m_selectedMusic0.AddListener(OnSelectedMusic0Changed);
-        m_selectedMusic1.AddListener(OnSelectedMusic1Changed);
+        m_selectMusicInput.AddIndexListener(OnIndexChanged);
         m_playMusicEvent.AddDownListener(OnPlayMusicChanged);
 
-        UpdateSelectedMusic();
+        m_selectedMusicIndex = m_selectMusicInput.Index.Value.Mod(m_states.Length);
     }
 
     private void Dispose()
     {
-        m_selectedMusic0.RemoveListener(OnSelectedMusic0Changed);
-        m_selectedMusic1.RemoveListener(OnSelectedMusic1Changed);
+        m_selectMusicInput.RemoveIndexListener(OnIndexChanged);
         m_playMusicEvent.RemoveListener(OnPlayMusicChanged);
     }
 
     private void OnPlayMusicChanged()
     {
-        m_playingMusic = !m_playingMusic;
-        if (m_playingMusic) 
+        // Button pressed on the same music => stop the music
+        if (m_selectedMusicIndex == m_playingMusicIndex)
         {
-            m_states[m_selectedMusic].SwitchToMusic(gameObject);
-            m_states[m_selectedMusic].SetIsPlaying(true);
+            Debug.Log($"Stopping music at index {m_selectedMusicIndex}");
+            m_states[m_selectedMusicIndex].SetIsPlaying(false);
+            m_playingMusicIndex = -1;
+            AudioManager.Instance.StateManager.SetMusicState(MusicState.None);
         }
         else
         {
-            AudioManager.Instance.StateManager.SetMusicState(MusicState.None);
-            m_states[m_selectedMusic].SetIsPlaying(false);
+            // Else, start the new music
+            if (m_playingMusicIndex != -1)
+            {
+                m_states[m_playingMusicIndex].SetIsPlaying(false);
+            }
+            Debug.Log($"Playing music at index {m_selectedMusicIndex}");
+            m_states[m_selectedMusicIndex].SetIsPlaying(true);
+            m_playingMusicIndex = m_selectedMusicIndex;
+            m_states[m_selectedMusicIndex].Play();
         }
     }
 
-    private void OnSelectedMusic1Changed(bool obj)
+    private void OnIndexChanged(int index)
     {
-        UpdateSelectedMusic();
+        m_selectedMusicIndex = index.Mod(m_states.Length);
     }
-
-    private void OnSelectedMusic0Changed(bool obj)
-    {
-        UpdateSelectedMusic();
-    }
-
-    private void UpdateSelectedMusic()
-    {
-        int newSelectedMusic = 0;
-        newSelectedMusic |= (m_selectedMusic0.Value ? (1 << 0) : 0);
-        newSelectedMusic |= (m_selectedMusic1.Value ? (1 << 1) : 0);
-
-        if (newSelectedMusic == m_selectedMusic)
-        {
-            return;
-        }
-
-        m_selectedMusic = newSelectedMusic;
-        // Stop music
-        m_states[m_selectedMusic].SetIsPlaying(false);
-
-        m_playingMusic = false;
-
-        AudioManager.Instance.StateManager.SetMusicState(MusicState.None);
-    }
-
 }
